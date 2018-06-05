4a872eff55bd12c602fd95ab6b024d792089f938
Isaias Pomales <impomales@gmail.com>
2018-06-05 15:27:17 -0400

    add props and validators to model

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,13 +1,45 @@
-const Sequelize = require("sequelize");
-const db = require("../db");
+const Sequelize = require('sequelize');
+const db = require('../db');
 
-const Product = db.define("product", {
+const Product = db.define('product', {
   title: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
       notEmpty: true
     }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true
+    }
+  },
+  category: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isDecimal: true
+    }
+  },
+  inventory: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      isInt: true
+    }
+  },
+  image: {
+    type: Sequelize.STRING,
+    defaultValue: 'http://via.placeholder.com/300x300'
   }
 });
 

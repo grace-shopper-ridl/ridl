fd2a49346fcd680a49d2bb65a79e83759b9b2fa8
richardsunny2015 <richardsunny2015@gmail.com>
2018-06-05 15:30:41 -0400

    our allProducts page shows our dummy data

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -3,6 +3,45 @@ import { connect } from 'react-redux';
 
 export default class AllProducts extends Component {
   render() {
-    return (<h1>This is our Products: </h1>);
+    const dummyProducts = [
+      {
+        id: 1,
+        name: 'Peanuts',
+        description: 'These are peanuts.',
+        price: 34.5,
+        inventory: 10
+      },
+      {
+        id: 2,
+        name: 'Cashews',
+        description: 'These are cashews.',
+        price: 32.5,
+        inventory: 1
+      },
+      {
+        id: 3,
+        name: 'Pecans',
+        description: 'These are pecans.',
+        price: 27.5,
+        inventory: 20
+      }
+    ];
+    const products = dummyProducts;
+
+    return (
+      <div>
+        <h1>This is our Products: </h1>
+        {products.map(product => {
+          return (
+            <div key={product.id}>
+              <h3>{product.name}</h3>
+              <h4>{product.description}</h4>
+              <p>{product.price}</p>
+              {product.inventory < 10 && <p>ONLY {product.inventory} LEFT</p>}
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }

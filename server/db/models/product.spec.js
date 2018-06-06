de8d46099504d6cb1890b0282badaa338a75fd18
Isaias Pomales <impomales@gmail.com>
2018-06-06 19:11:40 -0400

    add testing for throwing error

--- server/db/models/product.spec.js
+++ server/db/models/product.spec.js
@@ -4,20 +4,48 @@ const db = require('../index');
 const Product = db.model('product');
 
 describe('Product model', () => {
-  beforeEach(() => {
+  before(() => {
     return db.sync({ force: true });
   });
 
-  it('creates a product successfully', () => {
-    Product.create({
+  let peanut;
+  beforeEach(() => {
+    return Product.create({
       name: 'peanut',
       description: 'this is a peanut',
-      price: 5.00,
+      price: 5.0,
       inventory: 20
     })
-    .then(peanut => {
-      expect(peanut).to.be.an('object');
+      .then(created => {
+        peanut = created;
+      })
+      .catch(console.error);
+  });
+
+  it('creates a product successfully', () => {
+    expect(peanut).to.be.an('object');
+  });
+
+  it('has a name', () => {
+    expect(peanut.name).to.be.a('string');
+    expect(peanut.name).to.equal('peanut');
+  });
+
+  it('throws an error if price is not a number', () => {
+    Product.build({
+      name: 'cashew',
+      description: 'blah blah',
+      price: 'HA!',
+      inventory: 1
     })
-    .catch(console.error);
-  })
+      .validate()
+      .then(
+        () => {
+          throw new Error('price must be a number.');
+        },
+        result => {
+          expect(result).to.be.an.instanceOf(Error);
+        }
+      );
+  });
 });

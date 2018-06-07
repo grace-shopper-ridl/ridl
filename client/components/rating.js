321ab8cd434dc63a1e9b22214071449ad8b21d7f
richardsunny2015 <richardsunny2015@gmail.com>
2018-06-07 11:54:35 -0400

    removed console logs and comments

--- client/components/rating.js
+++ client/components/rating.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
-const image = 'https://cdn2.iconfinder.com/data/icons/flat-game-ui-buttons-icons-1/512/10-512.png'
-
 const Ratings = (props) => {
-    // let arrToBeMapped = new Array(props.rating)
-    // console.log(props.rating)
     let arr = [];
     let keys = 0;
     for (let i = 1; i <= props.rating; i++) arr.push(<img key={keys++} src="https://cdn2.iconfinder.com/data/icons/flat-game-ui-buttons-icons-1/512/10-512.png" />)

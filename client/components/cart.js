bd4bf64e1f6f7c32b12a772b5f22137ee3204413
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-06-09 14:37:02 -0400

    add price component to cart

--- client/components/cart.js
+++ client/components/cart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { changeItemQuantityThunk } from '../store';
 import Checkout from './stripeCheckout';
+import Price from './price';
 
 const Cart = props => {
   return (
@@ -11,7 +12,7 @@ const Cart = props => {
           <div key={lineItem.id} className="lineItem">
             <h3>{lineItem.product.name}</h3>
             <img src={lineItem.product.image} />
-            <p>${lineItem.price}</p>
+            <Price product={lineItem.product} />
             <label>
               Quantity: <input type="number" placeholder={lineItem.qty} />
             </label>

bb0781d60f2df82e43246505af0ea41403f6842b
richardsunny2015 <richardsunny2015@gmail.com>
2018-06-09 09:15:24 -0400

    bug(order put route): does not include products in the line items that are sent back when changing quantity

--- client/components/cart.js
+++ client/components/cart.js
@@ -3,19 +3,24 @@ import { connect } from 'react-redux';
 import { changeItemQuantityThunk } from '../store';
 
 const Cart = props => {
+  const cartId = props.cart.id;
+  const changeQty = props.changeQty;
   return (
-      <section id="cart">
-        {props.cart.lineItems.map(lineItem => (
-            <div key={lineItem.id} className="lineItem" >
-                <h3>{lineItem.product.name}</h3>
-                <img src={lineItem.product.image} />
-                <p>${lineItem.price}</p>
-                <label>Quantity: <input type="number" value={lineItem.qty} /></label>
-            </div>
-    ))}
-      <button>Checkout</button>
-      </section>
-    )
+    <section id="cart">
+      {props.cart.lineItems.length &&
+        props.cart.lineItems.map(lineItem => (
+          <div key={lineItem.id} className="lineItem">
+            <h3>{lineItem.product.name}</h3>
+            <img src={lineItem.product.image} />
+            <p>${lineItem.price}</p>
+            <label>
+              Quantity: <input type="number" value={lineItem.qty} />
+            </label>
+          </div>
+        ))}
+      <button type="submit">Checkout</button>
+    </section>
+  );
 };
 
 const mapStateToProps = state => ({
@@ -30,4 +35,4 @@ const mapDispatchToProps = dispatch => ({
 
 const ConnectedCart = connect(mapStateToProps, mapDispatchToProps)(Cart);
 
-export default ConnectedCart
+export default ConnectedCart;

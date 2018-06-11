64c76f13b72b33e350c44e36a08c981434b6d46a
richardsunny2015 <richardsunny2015@gmail.com>
2018-06-11 17:50:36 -0400

    feat(currentOrder): current order has a route

--- client/components/currentOrder.js
+++ client/components/currentOrder.js
@@ -1,2 +1,29 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import {fetchCurrentOrder} from '../store';
+
+class CurrentOrder extends Component {
+    componentDidMount() {
+        this.props.setOrder(this.props.match.params.orderId)
+    }
+    render() {
+        const {currentOrder} = this.props;
+        return (
+            <section>
+                <h1>{currentOrder.id}</h1>
+            </section>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    currentOrder: state.currentOrder
+})
+
+const mapDispatchToProps = dispatch => ({
+    setOrder: (orderId) => {
+        dispatch(fetchCurrentOrder(orderId))
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CurrentOrder)

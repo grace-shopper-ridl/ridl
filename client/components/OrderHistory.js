dc5f78ada1731a2e2876f7988be4cc069010adbe
Isaias Pomales <impomales@gmail.com>
2018-06-11 16:53:32 -0400

    feat(react): renders all order history of current user

--- client/components/OrderHistory.js
+++ client/components/OrderHistory.js
@@ -1,30 +1,30 @@
-import React, {Component} from 'react';
-import Price from 'price';
-import { connect} from 'react-redux';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const OrderHistory =  (
-  <section id="orders">
-    <h1 id="orders-header">Order History: </h1>
-    {
-      orderHistory && orderHistory.map(order => {
-        return (
-          <div key={order.id}>
-            <Link to={`/orders/${order.id}`}>
-              <h2 className="individual-order__id">
-                {order.id}
-              </h2>
-            </Link>
-            <h3>Status: {order.status}</h3>
-            <Price order={{product: order}} />
-          </div>
-        );
-      })
-    }
-  </section>
-);
+const OrderHistory = props => {
+  console.log('hi...');
+  return (
+    <section id="orders">
+      <h1 id="orders-header">Order History: </h1>
+      {props.orderHistory &&
+        props.orderHistory.map(order => {
+          return (
+            <div key={order.id}>
+              <Link to={`/orders/${order.id}`}>
+                <h2 className="individual-order__id">Order Number #{order.id}</h2>
+              </Link>
+              <h3>Status: {order.status}</h3>
+              <p>${order.subTotal / 100}</p>
+            </div>
+          );
+        })}
+    </section>
+  );
+};
 
 const mapStateToProps = state => ({
   orderHistory: state.orderHistory
-})
+});
 
 export default connect(mapStateToProps)(OrderHistory);
